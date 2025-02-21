6fd66e9c101e4d00ff7c585b8f4964dd77cc835f
karakusHalil <karakus_halil@outlook.com>
2025-02-21 18:53:58 +0300

    practice

---
+++ 02-REACT-FORM/rf02-todo-list/src/App.jsx
@@ -0,0 +1,18 @@
+import TaskForm from "./assets/TaskForm";
+
+function App() {
+  return (
+    <>
+      <div className="container bg-primary p-3">
+        <h1>To-Do List</h1>
+        <div className="row justify-content-center mt-5">
+          <div className="col-8 bg-danger p-3 rounded">
+            <TaskForm />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ 02-REACT-FORM/rf02-todo-list/src/assets/TaskForm.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+
+function TaskForm() {
+    const emptyForm = {task : "", important : false};
+  const [formData, setFormData] = useState(emptyForm);
+  const [tasks,setTasks] = useState([]);
+
+
+  function handleInputChange(e) {
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [e.target.name]:
+          e.target.type === "checkbox" ? e.target.checked : e.target.value,
+      };
+    });
+
+   // console.log(formData)
+
+    // e.target.type === "checkbox"
+    //   ? console.log(e.target.checked)
+    //   : console.log(e.target.value);
+  }
+
+  function handleButtonSubmit(e){
+    e.preventDefault();
+    setTasks((prev) => [...prev,formData])
+
+  //  console.log(formData)
+  }
+
+
+
+
+  return (
+    <>
+      <form onSubmit={handleButtonSubmit}>
+        <div className="row mb-3">
+          <label htmlFor="task" className="col-sm-2 col-form-label">
+            Task
+          </label>
+          <div className="col-sm-10">
+            <input
+              type="text"
+              className="form-control"
+              id="task"
+              name="task"
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+
+        <div className="row mb-3">
+          <div className="col-sm-10 offset-sm-2">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                id="important"
+                name="important"
+                onChange={handleInputChange}
+              />
+              <label className="form-check-label" htmlFor="gridCheck1">
+                Important
+              </label>
+            </div>
+          </div>
+        </div>
+        <button type="submit" className="btn btn-dark">
+          Save
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default TaskForm;
