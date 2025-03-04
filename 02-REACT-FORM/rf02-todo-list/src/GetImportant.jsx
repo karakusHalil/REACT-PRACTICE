37e9e09f727449edc3f5fc0e794d239469154f26
karakusHalil <karakus_halil@outlook.com>
2025-03-04 06:56:09 +0300

    Add Get Important

---
+++ 02-REACT-FORM/rf02-todo-list/src/GetImportant.jsx
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types";
+
+function GetImportant({handleTasksFilter, important}) {
+  return (
+    <>
+      <button className={`btn btn-sm ${!important ? "btn-warning" : "btn-info"} float-end`} onClick={handleTasksFilter}>
+        {!important ? "Get Importants" : "Get All Tasks"}
+      </button>
+    </>
+  );
+}
+
+export default GetImportant;
+
+
+GetImportant.propTypes = {
+    handleTasksFilter : PropTypes.func,
+    important : PropTypes.bool
+}
--- 02-REACT-FORM/rf02-todo-list/src/TaskList.jsx
+++ 02-REACT-FORM/rf02-todo-list/src/TaskList.jsx
@@ -1,15 +1,34 @@
 import PropTypes from "prop-types";
+import GetImportant from "./GetImportant";
+import { useState } from "react";
+import { useEffect } from "react";
+
 
 function TaskList({ tasks, removeTask, editTask }) {
+  
+  const [important,setImportant] = useState(false);
+  const [filteredTasks,setFilteredTasks] = useState(tasks);
+
+  function handleTasksFilter(){
+    const newImportant = !important;
+    newImportant ? setFilteredTasks(tasks.filter(item => item.important === true)) : setFilteredTasks(tasks)
+    setImportant(newImportant);
+  }
+
+  useEffect(()=>{
+    setFilteredTasks(tasks);
+  },[tasks])
+  
   if (tasks.length === 0) {
     return <></>;
   }
   return (
     <>
       <div>
-        <h4 className="mt-5">Task List</h4>
-        <ul className="list-group mt-3">
-          {tasks.map((item, index) => (
+        <h4 className="mt-5">Task List <GetImportant handleTasksFilter={handleTasksFilter} important={important}/></h4>
+        
+        <ul className="list-group mt-3 w100">
+          {filteredTasks.map((item, index) => (
             <li
               className="list-group-item d-flex justify-content-between"
               key={index}
