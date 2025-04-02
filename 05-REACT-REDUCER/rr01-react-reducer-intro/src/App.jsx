9407b6b9be497c3895dbafbf6cb645dfe08b992b
karakusHalil <karakus_halil@outlook.com>
2025-04-02 23:33:07 +0300

    .

--- 05-REACT-REDUCER/rr01-react-reducer-intro/src/App.jsx
+++ 05-REACT-REDUCER/rr01-react-reducer-intro/src/App.jsx
@@ -1,12 +1,42 @@
-
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>REDUCER</h1>
+      <h1>React Reducer</h1>
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <h2>1.Yöntem</h2>
+        <h3>Counter : {count}</h3>
+        <div>
+          <button onClick={() => setCount((prev) => prev - 1)}> - </button>
+          <button onClick={() => setCount(0)}>RESET</button>
+          <button onClick={() => setCount((prev) => prev + 1)}> + </button>
+        </div>
+      </div>
+      <hr />
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <h2>2.Yöntem</h2>
+        <h3>Counter : {count}</h3>
+        <div>
+          <button onClick={() => setCount((prev) => prev - 1)}> - </button>
+          <button onClick={() => setCount(0)}>RESET</button>
+          <button onClick={() => setCount((prev) => prev + 1)}> + </button>
+        </div>
+      </div>
+      <hr />
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <h2>3.Yöntem Reducer</h2>
+        <h3>Counter : {count}</h3>
+        <div>
+          <button onClick={() => setCount((prev) => prev - 1)}> - </button>
+          <button onClick={() => setCount(0)}>RESET</button>
+          <button onClick={() => setCount((prev) => prev + 1)}> + </button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
