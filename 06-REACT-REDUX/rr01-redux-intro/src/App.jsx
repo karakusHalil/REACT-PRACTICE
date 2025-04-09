1e24bd7364c152c92975813a259bfc01335b1d9e
karakusHalil <karakus_halil@outlook.com>
2025-04-10 01:44:39 +0300

    add useSelector

--- 06-REACT-REDUX/rr01-redux-intro/src/App.jsx
+++ 06-REACT-REDUX/rr01-redux-intro/src/App.jsx
@@ -1,13 +1,22 @@
-
+import { useSelector } from "react-redux";
 
 function App() {
-
-
+  const value = useSelector((store) => store.counter.value);
+  console.log(value);
   return (
     <>
       <h1>REDUX TOOLKIT</h1>
+      <h3>Counter : </h3>
+      <button>DECREMENT</button>
+      <button>INCREMENT</button>
+      <button>RESET</button>
+      <br />
+      <hr />
+      <br />
+      <input type="number" id="number" />
+      <button>ADDED</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
