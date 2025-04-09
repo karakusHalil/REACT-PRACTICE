d764a53a9c19293ff0989a20d64481771d76c4a7
karakusHalil <karakus_halil@outlook.com>
2025-04-10 01:35:22 +0300

    created counterSlice and reduxStore

---
+++ 06-REACT-REDUX/rr01-redux-intro/src/store/counter/counterSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = { value: 0 };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      console.log("increment", state);
+      state.value++;
+    },
+    decrement(state) {
+      console.log("decrement", state);
+      state.value--;
+    },
+    added(state, action) {
+      console.log("added", state, action);
+      state.value += action.payload.item;
+    },
+    reset(state) {
+      console.log(state);
+      return initialState;
+    },
+  },
+});
+
+export const { increment, decrement, added, reset } = counterSlice.actions;
+export default counterSlice.reducer;
