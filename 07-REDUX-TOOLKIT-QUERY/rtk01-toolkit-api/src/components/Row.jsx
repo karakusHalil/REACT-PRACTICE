97ccbf0ad68cb0bac0f055fa40676e135e761919
karakusHalil <karakus_halil@outlook.com>
2025-04-11 01:43:45 +0300

    .

---
+++ 07-REDUX-TOOLKIT-QUERY/rtk01-toolkit-api/src/components/Row.jsx
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types"
+
+function Row({children, className}) {
+  return (
+    <div className={`row ${className}`}>
+        {children}
+    </div>
+  )
+}
+
+export default Row
+
+Row.propTypes = {
+    children : PropTypes.element,
+    className : PropTypes.string
+}
