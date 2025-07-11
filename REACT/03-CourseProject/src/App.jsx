e3448282661bfcc299fb1bce42ca2fcd2fdd18c5
karakusHalil <karakus_halil@outlook.com>
2025-07-11 19:35:22 +0300

    completed

--- REACT/03-CourseProject/src/App.jsx
+++ REACT/03-CourseProject/src/App.jsx
@@ -1,13 +1,19 @@
 import Header from "./Header";
 import { courses } from "./Data";
 import Course from "./Course";
+
 function App() {
   return (
     <>
       <Header />
-      {courses?.map((course) => (
-        <Course key={course.id} course={course} />
-      ))}
+      <div
+        className="course-main"
+        style={{ display: "flex", flexDirection: "row", padding: "0 10px" }}
+      >
+        {courses?.map((course) => (
+          <Course key={course.id} course={course} />
+        ))}
+      </div>
     </>
   );
 }
--- REACT/03-CourseProject/src/Course.jsx
+++ REACT/03-CourseProject/src/Course.jsx
@@ -5,13 +5,11 @@ const Course = ({ course }) => {
   return (
     <>
       <div className="course">
-        <div>
-          <img src={image} alt={title} />
-          <h4>{title}</h4>
-          <h5>{description}</h5>
-          <h3>{price}</h3>
-          <a href={link}>Kurs Linki!</a>
-        </div>
+        <img className="img" src={image} alt={title} />
+        <h4>{title}</h4>
+        <p>{description}</p>
+        <h3>{price}TL</h3>
+        <a href={link}>Kurs Linki!</a>
       </div>
     </>
   );
