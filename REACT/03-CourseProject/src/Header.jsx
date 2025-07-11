3625567a63f7f33e53b7e45d41bd0415086b0372
karakusHalil <karakus_halil@outlook.com>
2025-07-11 18:33:49 +0300

    Add header component

---
+++ REACT/03-CourseProject/src/Header.jsx
@@ -0,0 +1,13 @@
+import "./css/Header.css";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <div className="title">UDEMY KURSLARI</div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
