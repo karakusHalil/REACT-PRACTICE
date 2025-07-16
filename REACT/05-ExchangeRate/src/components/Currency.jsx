69ca33deffce354a71b5fb827423769089fb2fa9
karakusHalil <karakus_halil@outlook.com>
2025-07-16 16:47:18 +0300

    Add button and css

--- REACT/05-ExchangeRate/src/components/Currency.jsx
+++ REACT/05-ExchangeRate/src/components/Currency.jsx
@@ -1,23 +1,31 @@
 import "../css/currency.css";
-import { FaArrowRightArrowLeft } from "react-icons/fa6";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const Currency = () => {
   return (
     <>
       <div className="currency-div">
-        <input type="number" className="amount" />
-        <select className="from-currency-option">
-          <option value="">USD</option>
-          <option value="">EUR</option>
-          <option value="">TL</option>
-        </select>
-        <FaArrowRightArrowLeft />
-        <select className="to-currency-option">
-          <option value="">TL</option>
-          <option value="">USD</option>
-          <option value="">EUR</option>
-        </select>
-        <input type="number" className="result" />
+        <div className="title">
+          <h3>DÖVİZ KURU</h3>
+        </div>
+        <div className="exchange">
+          <input type="number" className="amount" />
+          <select className="from-currency-option">
+            <option value="">USD</option>
+            <option value="">EUR</option>
+            <option value="">TL</option>
+          </select>
+          <FaArrowRightLong />
+          <select className="to-currency-option">
+            <option value="">TL</option>
+            <option value="">USD</option>
+            <option value="">EUR</option>
+          </select>
+          <input type="number" className="result" />
+        </div>
+        <div>
+          <button className="btn-exchange">Çevir</button>
+        </div>
       </div>
     </>
   );
