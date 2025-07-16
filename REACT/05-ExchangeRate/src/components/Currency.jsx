4baaf0e3eee5b060a8d592d581ccaea6417d095e
karakusHalil <karakus_halil@outlook.com>
2025-07-16 17:15:31 +0300

    Add states and exchange functionality

--- REACT/05-ExchangeRate/src/components/Currency.jsx
+++ REACT/05-ExchangeRate/src/components/Currency.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import "../css/currency.css";
 import { FaArrowRightLong } from "react-icons/fa6";
 
 const Currency = () => {
+  const [amount, setAmount] = useState(0);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("TL");
+  const [result, setResult] = useState(0);
+
+  const exchange = () => {
+    console.log(amount);
+    console.log(fromCurrency);
+    console.log(toCurrency);
+  };
+
   return (
     <>
       <div className="currency-div">
@@ -9,22 +21,43 @@ const Currency = () => {
           <h3>DÖVİZ KURU</h3>
         </div>
         <div className="exchange">
-          <input type="number" className="amount" />
-          <select className="from-currency-option">
-            <option value="">USD</option>
-            <option value="">EUR</option>
-            <option value="">TL</option>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            className="amount"
+          />
+
+          <select
+            className="from-currency-option"
+            defaultValue="USD"
+            onChange={(e) => setFromCurrency(e.target.value)}
+          >
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
+            <option value="TL">TL</option>
           </select>
           <FaArrowRightLong />
-          <select className="to-currency-option">
-            <option value="">TL</option>
-            <option value="">USD</option>
-            <option value="">EUR</option>
+          <select
+            className="to-currency-option"
+            defaultValue="TL"
+            onChange={(e) => setToCurrency(e.target.value)}
+          >
+            <option value="TL">TL</option>
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
           </select>
-          <input type="number" className="result" />
+          <input
+            type="number"
+            className="result"
+            value={result}
+            onChange={(e) => setResult(e.target.value)}
+          />
         </div>
         <div>
-          <button className="btn-exchange">Çevir</button>
+          <button onClick={exchange} className="btn-exchange">
+            Çevir
+          </button>
         </div>
       </div>
     </>
