7c212bf398bac384d16f9534c12c4123ddf6b3d7
karakusHalil <karakus_halil@outlook.com>
2025-07-17 21:08:41 +0300

    Add components

--- REACT/06-TODO/src/App.jsx
+++ REACT/06-TODO/src/App.jsx
@@ -1,9 +1,23 @@
 import TodoCreate from "./components/TodoCreate";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <>
-      <TodoCreate />
+      <div className="App">
+        <div
+          style={{
+            width: "500px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <TodoCreate />
+          <TodoList />
+        </div>
+      </div>
     </>
   );
 }
