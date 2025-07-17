896d124eb7ad8f4292a6f85427d8f014c89ae1e8
karakusHalil <karakus_halil@outlook.com>
2025-07-17 21:11:34 +0300

    Create todoList

---
+++ REACT/06-TODO/src/components/TodoList.jsx
@@ -0,0 +1,13 @@
+import Todo from "./Todo";
+
+const TodoList = () => {
+  return (
+    <>
+      <div style={{ width: "100%", marginTop: "50px" }}>
+        <Todo />
+      </div>
+    </>
+  );
+};
+
+export default TodoList;
