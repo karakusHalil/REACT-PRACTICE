a36a7c50eae1d81eecb0b992cbf04d990a99ef09
karakusHalil <karakus_halil@outlook.com>
2025-07-20 03:13:50 +0300

    Set up basic routing with path definitions using Routes and Route

--- REACT/10-ROUTER-DOM/src/App.jsx
+++ REACT/10-ROUTER-DOM/src/App.jsx
@@ -1,13 +1,24 @@
-import {Route, Routes} from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  
-
   return (
     <>
-
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
