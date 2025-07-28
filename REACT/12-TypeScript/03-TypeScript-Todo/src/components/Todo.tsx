5949c32b9b83d2514787d68430dd655e80950c2a
karakusHalil <karakus_halil@outlook.com>
2025-07-28 19:46:40 +0300

    Create Todo, TodoList and Add css

--- REACT/12-TypeScript/03-TypeScript-Todo/src/components/Todo.tsx
+++ REACT/12-TypeScript/03-TypeScript-Todo/src/components/Todo.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { IoMdRemoveCircleOutline } from "react-icons/io";
+import { FaCheck } from "react-icons/fa";
+import { FaEdit } from "react-icons/fa";
+import "../css/Todo.css";
 
 const Todo = () => {
   return (
-    <div>Todo</div>
-  )
-}
+    <div className="todo-main">
+      <div className="todo-text">Ben ilk todoyum...</div>
+      <div className="todo-icons">
+        <IoMdRemoveCircleOutline className="icons" />
+        <FaEdit className="icons" />
+      </div>
+    </div>
+  );
+};
 
-export default Todo
+export default Todo;
