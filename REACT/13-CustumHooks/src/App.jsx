d7db3a7b23029caa05dbc31f1c737629f492ea71
karakusHalil <karakus_halil@outlook.com>
2025-08-01 02:08:20 +0300

    Add useCounter hooks

--- REACT/13-CustumHooks/src/App.jsx
+++ REACT/13-CustumHooks/src/App.jsx
@@ -1,14 +1,19 @@
-
-import './App.css'
+import "./App.css";
+import useCounter from "./hooks/useCounter";
 
 function App() {
-
-
+  const { count, increase, decrease } = useCounter();
   return (
     <>
-      
+      <div className="counter">
+        <div>{count}</div>
+        <div>
+          <button onClick={increase}>Arttır</button>
+          <button onClick={decrease}>Azalt</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
