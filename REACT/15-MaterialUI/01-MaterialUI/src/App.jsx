6f246d7696305df2bb65a0fd35943c5cffcb7760
karakusHalil <karakus_halil@outlook.com>
2025-08-04 17:12:50 +0300

    feat: add and test Material UI Button components

--- REACT/15-MaterialUI/01-MaterialUI/src/App.jsx
+++ REACT/15-MaterialUI/01-MaterialUI/src/App.jsx
@@ -1,13 +1,14 @@
-
-import './App.css'
+import MUIButton from "./components/MUIButton";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-
+      <div>
+        <MUIButton />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- REACT/15-MaterialUI/01-MaterialUI/src/components/MUIButton.jsx
+++ REACT/15-MaterialUI/01-MaterialUI/src/components/MUIButton.jsx
@@ -1,8 +1,43 @@
-
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 const MUIButton = () => {
   return (
-    <div>MUIButton</div>
-  )
-}
+    <div>
+      <Stack spacing={3} direction="column" style={{ marginBottom: "50px" }}>
+        <Button variant="text">KAYDOL</Button>
+        <Button variant="contained">KAYDOL</Button>
+        <Button variant="outlined">KAYDOL</Button>
+        <Button disabled>disabled</Button>
+        <Button variant="contained" disableElevation>
+          Disable elevation
+        </Button>
+      </Stack>
+      <hr />
+      <Stack spacing={2} direction="column">
+        <Button color="primary" variant="contained">
+          Primary
+        </Button>
+        <Button color="error" variant="contained">
+          Error
+        </Button>
+        <Button color="info" variant="contained">
+          Info
+        </Button>
+        <Button color="secondary" variant="contained">
+          Secondary
+        </Button>
+        <Button color="success" variant="contained">
+          Success
+        </Button>
+      </Stack>
+      <Stack spacing={3} direction="column" style={{ marginTop: "50px" }}>
+        <Button startIcon={<AddCircleIcon />} variant="contained">
+          Add
+        </Button>
+      </Stack>
+    </div>
+  );
+};
 
-export default MUIButton
+export default MUIButton;
