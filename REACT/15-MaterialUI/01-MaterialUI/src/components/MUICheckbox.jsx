3147a7e331c2a327a1b8aea3169c1b8766a97204
karakusHalil <karakus_halil@outlook.com>
2025-08-04 18:34:14 +0300

    feat: add and test Material UI Checkbox components

--- REACT/15-MaterialUI/01-MaterialUI/src/components/MUICheckbox.jsx
+++ REACT/15-MaterialUI/01-MaterialUI/src/components/MUICheckbox.jsx
@@ -1,12 +1,64 @@
+import Checkbox from "@mui/material/Checkbox";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import Button from "@mui/material/Button";
+import FormLabel from "@mui/material/FormLabel";
+import { useState } from "react";
 
 const MUICheckbox = () => {
+  const [isConfirm, setIsConfirm] = useState(false);
+  console.log(isConfirm);
+  const submit = () => {
+    if (!isConfirm) {
+      alert("Sözleşme onaylayınız");
+    } else {
+      alert("Başarıyla Gönderildi ");
+    }
+  };
   return (
     <>
-        <div>
-            
-        </div>
+      <div>
+        {/* <FormControl component="fieldset">
+          <FormLabel>Eğitim Durumu</FormLabel>
+          <FormGroup>
+            <FormControlLabel
+              control={<Checkbox />}
+              label="İlkokul"
+              labelPlacement="end"
+            />
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label="Lise"
+              labelPlacement="end"
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label="Üniversite"
+              labelPlacement="end"
+            />
+          </FormGroup>
+        </FormControl> */}
+        <FormControl component="fieldset">
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={isConfirm}
+                  onChange={(e) => setIsConfirm(e.target.checked)}
+                />
+              }
+              label="Kullanım koşullarını kabul ediyorum"
+              labelPlacement="end"
+            />
+            <Button onClick={submit} variant="contained">
+              Formu Gönder
+            </Button>
+          </FormGroup>
+        </FormControl>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default MUICheckbox
+export default MUICheckbox;
