ace240214ddf8a78235d4fea42fb266f2c8dcaae
karakusHalil <karakus_halil@outlook.com>
2025-08-18 19:51:42 +0300

    Add react-icons and google icon

--- REACT/16-Firebase/src/pages/Auth.jsx
+++ REACT/16-Firebase/src/pages/Auth.jsx
@@ -1,7 +1,32 @@
+import "../css/Auth.css";
+import { FaGoogle } from "react-icons/fa";
+
 const Auth = () => {
   return (
     <>
-      <div>Auth</div>
+      <div className="auth">
+        <h3 className="auth-header">Giriş Yap / Kaydol</h3>
+        <div className="auth-input">
+          <input
+            className="input-email"
+            type="text"
+            placeholder="Email giriniz..."
+          />
+          <input
+            className="input-password"
+            type="password"
+            placeholder="Şifre giriniz..."
+          />
+        </div>
+        <div className="auth-button">
+          <button className="button-google">
+            <FaGoogle />
+            Google ile GİRİŞ
+          </button>
+          <button className="button-login">Giriş Yap</button>
+          <button className="button-register">Kaydol</button>
+        </div>
+      </div>
     </>
   );
 };
