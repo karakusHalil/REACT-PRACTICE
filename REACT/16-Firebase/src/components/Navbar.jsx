56f0dd7ce896149d290ef5f3422ee56b50ce0cf1
karakusHalil <karakus_halil@outlook.com>
2025-08-18 21:05:54 +0300

    Add signOut

--- REACT/16-Firebase/src/components/Navbar.jsx
+++ REACT/16-Firebase/src/components/Navbar.jsx
@@ -1,11 +1,31 @@
+import { signOut } from "firebase/auth";
 import "../css/Navbar.css";
+import { auth } from "../Firebase";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+      toast.success("Çıkış yapıldı");
+      navigate("/auth");
+    } catch (error) {
+      toast.error("Çıkış yapılamadı !");
+    }
+  };
   return (
     <>
       <div className="navbar">
-        <div className="navbar-left">FİREBASE</div>
-        <div className="navbar-right">Çıkış yap</div>
+        <div onClick={() => navigate("/")} className="navbar-left">
+          FİREBASE
+        </div>
+        <div onClick={logout} className="navbar-right">
+          <button className="loggingOut">Çıkış yap</button>
+        </div>
       </div>
     </>
   );
